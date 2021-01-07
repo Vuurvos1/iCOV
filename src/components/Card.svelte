f0f9e6a2f1c273176fc5bcc9dee52e3e7a16cdde
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2021-01-07 12:42:32 +0100

    drag semi working?

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,10 +1,17 @@
 <script>
   let focused = false;
+  let initialX = 0;
+  let initialY = 0;
+  let startX = 0;
+  let startY = 0;
   let x = 0;
   let y = 0;
 
   function cardClick(e) {
     focused = true;
+
+    startX = e.offsetX;
+    startY = e.offsetY;
 
     x = e.offsetX;
     y = e.offsetY;
@@ -12,9 +19,20 @@
 
   function cardDrag(e) {
     if (focused) {
+      e.preventDefault();
+
+      console.log(e.offsetX);
+
+      const newX = e.offsetX - startX;
+      const newY = e.offsetY - startY;
+
       // e.target / e.srcElement
-      console.log(e.);
+      e.target.style.transform = `translate(${newX}px, ${newY}px)`;
     }
+  }
+
+  function cardEnd() {
+    focused = false;
   }
 </script>
 
@@ -28,6 +46,10 @@
   }
 </style>
 
-<div class="card" on:mousedown={cardClick} on:mousemove={cardDrag}>
+<div
+  class="card"
+  on:mousedown={cardClick}
+  on:mousemove={cardDrag}
+  on:mouseup={cardEnd}>
   <h3>Card title</h3>
 </div>
